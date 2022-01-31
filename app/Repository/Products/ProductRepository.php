1d965313ea6630b9ff10df108bd7dd4ace93319f
MostafaAlii <mostafa0alii@gmail.com>
2022-01-31 19:58:52 +0200

    start store general product information

--- app/Repository/Products/ProductRepository.php
+++ app/Repository/Products/ProductRepository.php
@@ -3,19 +3,25 @@ namespace App\Repository\Products;
 use App\Interfaces\Products\ProductRepositoryInterface;
 use App\Models\Section;
 use App\Models\Supplier;
+use App\Models\Servprice;
 use App\Models\Product;
 use Illuminate\Support\Facades\DB;
 class ProductRepository implements ProductRepositoryInterface {
     public function  index() {
         $data = [];
         $data['suppliers'] = Supplier::activeStatus()->select('id')->get();
-        //$data['sections'] = Section::get();
-        $data['products'] = Product::with('ProductSections','productAppointments', 'productServicePrices')->get();
+        $data['sections'] = Section::get();
+        $data['products'] = Product::all();
+        //$data['products'] = Product::with('ProductSections','productAppointments', 'productServicePrices')->get();
         return view('Dashboard.Products.index', $data);
     }
 
     public function create() {
-        return view('Dashboard.Products.btn.create');
+        $data = [];
+        $data['suppliers'] = Supplier::activeStatus()->select('id')->get();
+        $data['sections'] = Section::get();
+        $data['servprices'] = Servprice::activeStatus()->select('id')->get();
+        return view('Dashboard.Products.btn.create', $data);
     }
 
     public function store($request) {
