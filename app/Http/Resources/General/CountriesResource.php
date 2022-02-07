6ee51da3771c9da709bb12a9115496baf2e3b0e1
MostafaAlii <mostafa0alii@gmail.com>
2022-02-07 15:41:06 +0200

    finish Country & Proviences Api

--- app/Http/Resources/General/CountriesResource.php
+++ app/Http/Resources/General/CountriesResource.php
@@ -4,7 +4,8 @@ use Illuminate\Http\Resources\Json\JsonResource;
 class CountriesResource extends JsonResource {
     public function toArray($request) {
         return [
-
+            'id'                                    =>          $this->id,
+            'country_translations_name'             =>          $this->translations->keyBy('locale')->map->only('name'),
         ];
     }
 }
